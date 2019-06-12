0adbc4ae4eaad2400558f803cd76d53b104c1b65
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-06-12 22:14:29 +0530

    Update 3.py

--- code/3.py
+++ code/3.py
@@ -4,6 +4,6 @@ if (d>=97 and d<=122) or (d>=65 and d<=90):
     if c=='a' or c=='e' or c=='i' or c=='o' or c=='u'or c=='A' or c=='E' or c=='I' or c=='O' or c=='U':
         print('Vowel')
     else:
-        print('Consonants')
+        print('Consonant')
 else:
     print('invalid')
