6cf5d4835183e39033e20a5122df08bd6aa4a672
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-08 14:14:33 +0530

    Create hun2.py

---
+++ hun2.py
@@ -0,0 +1,14 @@
+n=int(input())
+l=[]
+for i in range(n):
+    m=input()
+    l.append(m)
+l.sort(reverse=True)
+k=set(l)
+o=list(k)
+if len(o)==1:
+    if o[0]=='0':
+        print('0')
+else:
+    for j in l:
+        print(str(j), end="")
