f2cd921db16918e7a09bb2fec2cfea3b5002f66a
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-09 12:14:57 +0530

    Update pro5.py

--- pro5.py
+++ pro5.py
@@ -6,13 +6,18 @@ a=int(l[1])
 b=int(l[2])
 i=0
 c=0
-while i<n:
-    for j in range(1,n):
-        i=a*j+b*j
-        if i==n:
-            print("YES")
-            c=1
+while i<=n:
+    for j in range(0,n):
+        for k in range(0,n):
+            i=(a*j+b*k)*2
+            if i==n:
+                print("YES")
+                c=1
+                break
+        if c==1:
             break
     if c==0:
         print("NO")
+        break
+    else:
         break
