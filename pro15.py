acfbedb6d92a011207334b355f27c2aaefeed64d
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-12 21:26:27 +0530

    Create pro15.py

---
+++ pro15.py
@@ -0,0 +1,14 @@
+import math
+o=input()
+n=input()
+m=n.split(" ")
+p=[]
+for i in m:
+    p.append(int(i))
+q=max(p)
+s=int(math.ceil(q**0.5))
+print(q, s)
+for j in range(s,0,-1):
+    for k in range(1,s):
+        if pow(2,j)-k in p:
+            print(pow(2,j)-k)
