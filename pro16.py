249eb8bcbcf1bce6d00e2fc74a9b5d813a6e3bbf
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-12 23:11:08 +0530

    Create pro16.py

---
+++ pro16.py
@@ -0,0 +1,20 @@
+w=int(input())
+n=input()
+m=n.split(" ")
+l=[]
+for i in m:
+    l.append(int(i))
+l.sort()
+count=0
+#print(l)
+b=1
+res=0
+z=[]
+for j in range(0,w):
+    for k in l:
+        if not(k in z):
+            z.append(k)
+            count=l.count(k)
+            res+=count*b
+            b+=1
+print(res)
