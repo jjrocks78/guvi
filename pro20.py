5a10c30a22e2844eec12b1374f27f6e18af32e59
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-13 22:06:56 +0530

    Create pro20.py

---
+++ pro20.py
@@ -0,0 +1,18 @@
+n=input()
+m=n.split(" ")
+M=int(m[1])
+o=input()
+p=o.split(" ")
+q=[]
+for i in p:
+    q.append(int(i))
+q.sort(reverse=True)
+count=0
+for j in range(0,len(q)):
+    if M>=q[j]:
+        a=M//q[j]
+        M=M-a*q[j]
+        count+=a
+    elif M==0:
+        print(count)
+        break
