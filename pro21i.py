d26c7cda1a06563033f32e70cdfea876d62b6cef
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-14 19:07:04 +0530

    Create pro21i.py

---
+++ pro21i.py
@@ -0,0 +1,20 @@
+from statistics import mean
+n=int(input())
+m=input()
+p=m.split(" ")
+q=[]
+av1=0
+av2=0
+b=0
+for i in p:
+    q.append(int(i))
+for j in range(1,len(q)):
+    av1=sum(q[:j])/len(q[:j])
+    av2= sum(q[j:]) / len(q[j:])
+    #print(av1, av2)
+    if int(av1)==int(av2):
+        print("yes")
+        b=1
+        break
+if b==0:
+    print("no")
