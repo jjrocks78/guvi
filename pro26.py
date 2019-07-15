a28ad98f0b9b71dd2567f925a0b840f483efcf05
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-15 18:46:53 +0530

    Update pro26.py

--- pro26.py
+++ pro26.py
@@ -1,19 +1,19 @@
-n=int(input())
-m=input()
-o=m.split(" ")
+num=int(input())
+mn=input()
+a=mn.split(" ")
 p=[]
-for i in o:
+for i in a:
     p.append(int(i))
 q=[]
-count=1
-for j in range(0,n-1):
+c=1
+for j in range(0,num-1):
     if p[j]<p[j+1]:
-        count+=1
+        c+=1
     else:
         q.append(count)
         #print(q)
-        count=1
+        c=1
     #print(j,count)
-q.append(count)
+q.append(c)
 print(max(q))
 
