6c0e0dd956712e487c80696fe6258bfc170c5852
jjrocks78 <51355738+jjrocks78@users.noreply.github.com>
2019-07-24 18:46:21 +0530

    Update hun6.py

--- hun6.py
+++ hun6.py
@@ -2,10 +2,14 @@ a=int(input())
 b=input()
 c=b.split(" ")
 d=[]
+x=0
 for i in range(0,a):
     if c[i]  in d:
         print(c[i])
+        x=1
         break
     else:
         d.append(c[i])
 
+if x==0:
+    print("unique")
